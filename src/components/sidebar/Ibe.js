ddb83487eb524de9ef60c11ab7d74df4fc85177e
CodingBgirl <bgirlapollo@gmail.com>
2021-04-14 11:14:43 -0400

    Just to save work DO NOT MERGE YETgit add .

--- src/components/sidebar/Ibe.js
+++ src/components/sidebar/Ibe.js
@@ -1,26 +1,52 @@
 import React, { useState, useEffect } from "react";
 import { Accordion, Button, Card } from "react-bootstrap";
-
+import "./IbeAPI.css";
 // https://github.com/public-apis/public-apis
 
 function Ibe() {
-  // useEffect(() => {}, []);
+  const [dogPics, setDogPics] = useState();
 
-  // Add the API fetch request here
-
+  useEffect(() => {
+    // Add the API fetch request here
+    fetch("https://random.dog/woof.json")
+      .then((res) => res.json())
+      .then((data) => {
+        setDogPics(data.url);
+      });
+  }, []);
   // Add other functions here
+
+  // function Doggie(event) {
+  //   console.log("Fuck Yeah!!");
+  //   if (event === true) {
+  //     fetch("https://random.dog/woof.json")
+  //       .then((res) => res.json())
+  //       .then((data) => {
+  //         setDogPics(data.url);
+  //       });
+  //   }
+  // }
+
+  function newDog(event){
+    console.log("Fuck Yeah!!");
+  }
 
   return (
     <Accordion>
       <Card>
         <Card.Header>
           <Accordion.Toggle as={Button} variant="link" eventKey="0">
-            Ibe API
+            Random Dog Picture
           </Accordion.Toggle>
         </Card.Header>
         <Accordion.Collapse eventKey="0">
           <Card.Body>
-            <section>ADD FUNCTIONS OR TEXT HERE</section>
+            <section>
+              <div>
+                <img src={dogPics} />
+                <button onClick={newDog}>New Picture</button>
+              </div>
+            </section>
           </Card.Body>
         </Accordion.Collapse>
       </Card>
